ca794c921fc298af25c43a5217f9c4e70a07388d
mishelka <bacikova.michaela@gmail.com>
2018-04-12 09:29:34 +0200

    Zmeneny Minesweeper

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -3,9 +3,6 @@ package minesweeper;
 import minesweeper.consoleui.ConsoleUI;
 import minesweeper.core.Field;
 
-/**
- * Main application class.
- */
 public class Minesweeper {
     /** User interface. */
     private ConsoleUI userInterface;
@@ -16,8 +13,9 @@ public class Minesweeper {
     private Minesweeper() {
         userInterface = new ConsoleUI();
         
-        Field field = new Field(9, 9, 10);
+        Field field = new Field(8, 8, 6);
         userInterface.newGameStarted(field);
+        System.out.println("Hra zacala");
     }
 
     /**
